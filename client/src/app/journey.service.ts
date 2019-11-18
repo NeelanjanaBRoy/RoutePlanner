fbde3028f40710ca79faf504eb805066cda0b5ee
Neelanjana BasuRoy <neelanjana.basuroy@gmail.com>
2019-11-18 10:40:19 +0530

    UI fixes

--- client/src/app/journey.service.ts
+++ client/src/app/journey.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions  } from '@angular/http';
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JourneyService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public getRoutes(routeDetails){
+    let finalUrl = "https://api.tfl.gov.uk/journey/journeyresults/"+routeDetails.source+"/to/"+routeDetails.destination;
+    return this.httpClient.get(finalUrl).pipe(tap(response => {
+      return response;
+    }));
+  }
 }
--- client/src/app/journey/journey.component.ts
+++ client/src/app/journey/journey.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { JourneyService } from '../journey.service';
 
 @Component({
   selector: 'app-journey',
   templateUrl: './journey.component.html',
   styleUrls: ['./journey.component.scss']
 })
-export class JourneyComponent implements OnInit {
+export class JourneyComponent implements OnInit { 
+  public source: any;
+  public destination: any;
+  public routeDetails: any;
+  public data: any;
+  public destinationDisambiguation:any;
+  public sourceDisambiguation: any;
 
-  constructor() { }
+  constructor(public journeyService: JourneyService) { }
 
   ngOnInit() {
   }
+
+  getJourneys(){
+    this.routeDetails = {
+      source: this.source,
+      destination: this.destination
+    }
+    this.journeyService.getRoutes(this.routeDetails).subscribe(response => {
+      this.data = response;
+      console.log(this.data);
+      this.routeDetails = this.data.journeys;
+      this.destinationDisambiguation = '';
+      this.sourceDisambiguation = '';
+      
+    }, (err) => {
+        // console.log(err.error);
+        if (err.error.toLocationDisambiguation.disambiguationOptions){
+          this.destinationDisambiguation = err.error.toLocationDisambiguation.disambiguationOptions;
+        }
+        if (err.error.fromLocationDisambiguation.disambiguationOptions){
+          this.sourceDisambiguation = err.error.fromLocationDisambiguation.disambiguationOptions;
+        }
+        console.log("Multiple sources:", this.sourceDisambiguation);
+        console.log("Multiple destinations:",this.destinationDisambiguation);
+    });
+
+  }
+
+  onDestinationChange(value){
+    console.log(" Value is : ", value.srcElement.defaultValue);
+    this.destination = value.srcElement.defaultValue;
+  }
+
+  onSourceChange(value){
+    console.log(" Value is : ", value.srcElement.defaultValue );
+    this.source = value.srcElement.defaultValue;
+  }
+
 
 }
